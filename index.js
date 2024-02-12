09526cda7e1d982a6e7aaa03ccaca38db9a26ea0
abr-ya <gamer05@list.ru>
2024-02-13 01:30:16 +0300

    feat: Add Mongo DB Config

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
 const app = express();
 dotenv.config();
 
+const mongo_url = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.4ajdzvq.mongodb.net/`;
+
+const connect = async () => {
+  try {
+    await mongoose.connect(mongo_url);
+    console.log("Connected to mongoDB.");
+  } catch (error) {
+    throw error;
+  }
+};
+
+mongoose.connection.on("disconnected", () => {
+  console.log("mongoDB disconnected!");
+});
+
 // middlewares
 app.use(cors())
 app.use(cookieParser())
 app.use(express.json());
 
-app.use((err, req, res, next) => {
+app.use((err, _req, res, _next) => {
   const errorStatus = err.status || 500;
   const errorMessage = err.message || "Something went wrong!";
 
@@ -24,5 +40,6 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(8800, () => {
+  connect();
   console.log("Start Express on 8800 port!");
 });
