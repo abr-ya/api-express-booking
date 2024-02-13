82eceda48063c5535f8245c16ae78a99c79f99d1
abr-ya <gamer05@list.ru>
2024-02-13 14:40:21 +0300

    feat: Add routes

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ import mongoose from "mongoose";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
+import authRoute from "./routes/auth.js";
+import usersRoute from "./routes/users.js";
+import hotelsRoute from "./routes/hotels.js";
+import roomsRoute from "./routes/rooms.js";
+
 const app = express();
 dotenv.config();
 
@@ -26,6 +31,11 @@ mongoose.connection.on("disconnected", () => {
 app.use(cors())
 app.use(cookieParser())
 app.use(express.json());
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/hotels", hotelsRoute);
+app.use("/api/rooms", roomsRoute);
 
 app.use((err, _req, res, _next) => {
   const errorStatus = err.status || 500;
