978d7afbb9077054cc485bfb85cc26875466957c
abr-ya <gamer05@list.ru>
2024-02-13 19:56:35 +0300

    feat: Add hotel model and controller

--- routes/hotels.js
+++ routes/hotels.js
@@ -1,9 +1,28 @@
 import express from "express";
 
+import {
+  createHotel,
+  deleteHotel,
+  updateHotel,
+  getHotels,
+  getHotel,
+} from "../controllers/hotel.js";
+
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Hello from Hotels!");
-});
+// GET ALL
+router.get("/", getHotels);
+
+// CREATE
+router.post("/", createHotel);
+
+// UPDATE
+router.put("/:id", updateHotel);
+
+// DELETE
+router.delete("/:id", deleteHotel);
+
+// GET
+router.get("/find/:id", getHotel);
 
 export default router;
