ce6ff078504d64f7b40dabcda6197b61a6d7f168
abr-ya <gamer05@list.ru>
2024-02-14 16:36:02 +0300

    feat: Add room model and controller

--- routes/rooms.js
+++ routes/rooms.js
@@ -1,9 +1,24 @@
 import express from "express";
 
+import { createRoom, deleteRoom, getRoom, getRooms, updateRoom, updateRoomAvailability } from "../controllers/room.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
+
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Hello from Rooms!");
-});
+// GET == open
+router.get("/:id", getRoom);
+
+// GET ALL == open
+router.get("/", getRooms);
+
+// CREATE
+router.post("/:hotelid", verifyAdmin, createRoom);
+
+// UPDATE
+router.put("/:id", verifyAdmin, updateRoom);
+router.put("/availability/:id", updateRoomAvailability);
+
+// DELETE
+router.delete("/:id/:hotelid", verifyAdmin, deleteRoom);
 
 export default router;
