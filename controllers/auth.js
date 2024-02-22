48f545497c11d3a501be4bca96a5a025217bc009
abr-ya <gamer05@list.ru>
2024-02-22 08:50:18 +0300

    feat: Add user data to register response

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,18 +4,28 @@ import bcrypt from "bcryptjs";
 import User from "../models/User.js";
 import { createError } from "../utils/error.js";
 
+// for register and login
+const createCookieAndJson = (user) => {
+  const token = jwt.sign({ id: user._id, isAdmin: user.isAdmin }, process.env.SECRET);
+
+  const { isAdmin, email, _id: id, username } = user._doc;
+
+  return ({
+    cookie: ["access_token", token, { httpOnly: true, secure: false }],
+    json: { details: { id, email, username }, isAdmin, token } // токен дублируем == можно работать и так, и так)
+  })
+}
+
 export const register = async (req, res, next) => {
   try {
     const salt = bcrypt.genSaltSync(10);
     const hash = bcrypt.hashSync(req.body.password, salt);
-
-    const newUser = new User({
-      ...req.body,
-      password: hash,
-    });
-
+    const newUser = new User({ ...req.body, password: hash });
     await newUser.save();
-    res.status(200).send("User has been created.");
+
+    const { cookie, json } = createCookieAndJson(newUser);
+
+    res.cookie(...cookie).status(200).json(json);
   } catch (err) {
     next(err);
   }
@@ -24,24 +34,14 @@ export const register = async (req, res, next) => {
 export const login = async (req, res, next) => {
   try {
     const user = await User.findOne({ username: req.body.username });
-
     if (!user) return next(createError(404, "User not found!"));
 
-    const isPasswordCorrect = await bcrypt.compare(
-      req.body.password,
-      user.password
-    );
+    const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password);
+    if (!isPasswordCorrect) return next(createError(400, "Wrong password or username!"));
 
-    if (!isPasswordCorrect)
-      return next(createError(400, "Wrong password or username!"));
+    const { cookie, json } = createCookieAndJson(user);
 
-    const token = jwt.sign({ id: user._id, isAdmin: user.isAdmin }, process.env.SECRET);
-
-    const { isAdmin, email, _id: id, username } = user._doc;
-    res
-      .cookie("access_token", token, { httpOnly: true, secure: false })
-      .status(200)
-      .json({ details: { id, email, username }, isAdmin, token }); // токен тут избыточен, пока в куки)
+    res.cookie(...cookie).status(200).json(json);
   } catch (err) {
     next(err);
   }
